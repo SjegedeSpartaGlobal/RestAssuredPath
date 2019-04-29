ffed63616d0d778a6b7913542464d9835234a64c
Samuel Jegede <SJegede@spartaglobal.com>
2019-04-29 14:16:19 +0100

    first commit

---
+++ src/test/java/com/spartaglobal/rest_assured/AppTest.java
@@ -0,0 +1,20 @@
+package com.spartaglobal.rest_assured;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
